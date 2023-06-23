42daa358cab5bd6551f1ee5202a94f6d1b0d371c
Revand <revandaustin@gmail.com>
2023-06-23 23:33:51 +0700

    minor fix

--- controllers/postcontroller/postcontroller.go
+++ controllers/postcontroller/postcontroller.go
@@ -1,1 +1,42 @@
 package postcontroller
+
+import (
+	"errors"
+	"go-api/config"
+	"go-api/helper"
+	"go-api/models"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	var post []models.Post
+
+	if err := config.DB.Find(&post).Error; err != nil {
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helper.Response(w, 200, "List of Posts", post)
+
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	slug := mux.Vars(r)["slug"]
+
+	var post models.Post
+
+	if err := config.DB.First(&post, "slug = ?", slug).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helper.Response(w, 404, "Post not found", nil)
+			return
+		}
+
+		helper.Response(w, 500, err.Error(), nil)
+		return
+	}
+
+	helper.Response(w, 200, "Post Detail", post)
+}
